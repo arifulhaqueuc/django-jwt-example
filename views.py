b2b2974c9dd1ecf291a5193d07f1129241a4b7f2
Ariful Haque <arifulhaque2010@yahoo.com>
2023-08-09 17:00:09 -0400

    Create views.py

---
+++ views.py
@@ -0,0 +1,31 @@
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .models import UserProfile
+from rest_framework_jwt.utils import jwt_payload_handler, jwt_encode_handler
+
+@api_view(['POST'])
+def register_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    bio = request.data.get('bio', '')
+
+    if not username or not password:
+        return Response({'error': 'Username and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.create(username=username)
+    user.set_password(password)
+    user.save()
+
+    UserProfile.objects.create(user=user, bio=bio)
+
+    return Response({'message': 'User registered successfully.'}, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def protected_view(request):
+    user = request.user
+    user_profile = UserProfile.objects.get(user=user)
+    return Response({'message': 'This is a protected view.', 'bio': user_profile.bio})
